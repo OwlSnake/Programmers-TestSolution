735d7d2710ccfbaf32a5f6cc5aeb25fa47ba0d2e
imnhk <koloke1712@gmail.com>
2019-10-27 17:59:21 +0900

    DP

---
+++ baekjoon/2579_climb_stairs.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int pt[301], ptDP[301];
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	int N;
+	cin >> N;
+	
+	for (int i = 1; i <= N; i++)
+		cin >> pt[i];
+
+	ptDP[1] = pt[1];
+	ptDP[2] = ptDP[1] + pt[2];
+	ptDP[3] = max(ptDP[1] + pt[3], pt[2] + pt[3]);
+
+	for (int i = 4; i <= N; i++)
+		ptDP[i] = max(ptDP[i - 2] + pt[i], ptDP[i - 3] + pt[i - 1] + pt[i]);
+
+	cout << ptDP[N];
+	return 0;
+}
---
+++ baekjoon/9461_padovan_sequence.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+long long fib[101] = { 0, 1, 1, 1, 2, 2, 3, 4, 5, 7, 9 };
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	int T, N;
+	cin >> T;
+
+	for (int i = 11; i <= 100; i++)
+		fib[i] = (fib[i - 2] + fib[i - 3]);
+
+	for (int i = 0; i < T; i++)
+	{
+		cin >> N;
+		cout << fib[N] << '\n';
+	}
+	return 0;
+}
