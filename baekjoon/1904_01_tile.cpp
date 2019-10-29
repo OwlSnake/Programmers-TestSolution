4b6427422ad54c100f14d324b0cc5e0b8bf2e7b3
imnhk <koloke1712@gmail.com>
2019-10-29 17:39:02 +0900

    DP problems

---
+++ baekjoon/1904_01_tile.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int fib[1000001] = { 0, 1, 1, 3, 5 };
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	int N;
+	cin >> N;
+	for (int i = 4; i < N; i++)
+		fib[i + 1] = (fib[i] + fib[i - 1]) % 15746;
+
+	cout << fib[N];
+}
