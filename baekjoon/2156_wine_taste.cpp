1b1804e30305ed5557d6b6288ded9ac17bfef4da
imnhk <koloke1712@gmail.com>
2019-10-31 22:20:39 +0900

    Create 2156_wine_taste.cpp

---
+++ baekjoon/2156_wine_taste.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int drink[10001];
+int DP[10001];
+
+int max(int a, int b, int c) {
+	return max(a, max(b, c));
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+		cin >> drink[i];
+
+	DP[0] = 0;
+	DP[1] = drink[1];
+	DP[2] = drink[1] + drink[2];
+	for (int i = 3; i <= N; i++)
+		DP[i] = max(DP[i - 3] + drink[i - 1] + drink[i], DP[i - 2] + drink[i], DP[i - 1]);
+
+	cout << DP[N];
+	return 0;
+}
