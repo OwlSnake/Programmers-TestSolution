8947b7627e1495fbb5612e0490a5c6457c12ace2
imnhk <koloke1712@gmail.com>
2019-11-06 16:57:09 +0900

    Create 1912_continuous_sum.cpp

---
+++ baekjoon/1912_continuous_sum.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, num, answer = -1000;
+int DP[100001] = { 0 };
+
+int main() 
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> N;
+
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> num;
+		DP[i] = max(DP[i - 1] + num, num);
+		answer = max(answer, DP[i]);
+	}
+
+	cout << answer;
+	return 0;
+}
