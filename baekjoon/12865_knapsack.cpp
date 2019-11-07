86d6f4abf1460ebfc126cb8b7298df4bdd62d623
imnhk <koloke1712@gmail.com>
2019-11-07 16:07:02 +0900

    Create 12865_knapsack.cpp

---
+++ baekjoon/12865_knapsack.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int N, max_W;
+int DP[101][100001];
+int w[101], v[101];
+
+int max(int a, int b) {
+	return (a > b) ? a : b;
+}
+
+int main() 
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> N >> max_W;
+	for (int i = 1; i <= N; i++)
+		cin >> w[i] >> v[i];
+
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 1; j <= max_W; j++)
+		{
+			if (w[i] > j)
+				DP[i][j] = DP[i - 1][j];
+			else
+				DP[i][j] = max(DP[i - 1][j - w[i]] + v[i], DP[i - 1][j]);
+		}
+	}
+
+	cout << DP[N][max_W];
+	return 0;
+}
