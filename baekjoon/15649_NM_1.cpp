dce1e9d3a83350a62eae7bc0824f0c379735970f
imnhk <koloke1712@gmail.com>
2019-11-10 18:52:57 +0900

    Backtracking

---
+++ baekjoon/15649_NM_1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M;
+vector<int> stack;
+
+bool StackHas(int num) {
+	for (int i = 0; i < stack.size(); i++)
+		if (stack[i] == num)
+			return true;
+	return false;
+}
+
+void PrintStack() {
+	for (int i = 0; i < stack.size(); i++)
+		cout << stack[i] << ' ';
+	cout << '\n';
+}
+
+void BackTracking() {
+	if (stack.size() == M) {
+		PrintStack();
+		stack.pop_back();
+		return;
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		if (!StackHas(i)) {
+			stack.push_back(i);
+			BackTracking();
+		}
+	}
+	if(stack.size() > 0)
+		stack.pop_back();
+}
+
+int main() 
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> N >> M;
+
+	BackTracking();
+
+	return 0;
+}
---
+++ baekjoon/15650_NM_2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M;
+vector<int> stack;
+
+void PrintStack() {
+	for (int i = 0; i < stack.size(); i++)
+		cout << stack[i] << ' ';
+	cout << '\n';
+}
+
+void BackTracking() {
+	if (stack.size() == M) {
+		PrintStack();
+		stack.pop_back();
+		return;
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		if (stack.size() == 0 || stack.back() < i) {
+			stack.push_back(i);
+			BackTracking();
+		}
+	}
+	if(stack.size() > 0)
+		stack.pop_back();
+}
+
+int main() 
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> N >> M;
+
+	BackTracking();
+
+	return 0;
+}
