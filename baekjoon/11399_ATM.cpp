9673e19eefa35604de407e1b563dd4ca632ead88
imnhk <koloke1712@gmail.com>
2019-11-15 10:49:49 +0900

    Greedy

---
+++ baekjoon/11399_ATM.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, P[1001], answer = 0;
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		cin >> P[i];
+	sort(P, P + N);
+
+	for (int i = 0; i < N - 1; i++) {
+		P[i + 1] += P[i];
+		answer += P[i];
+	}
+	answer += P[N - 1];
+	cout << answer;
+	return 0;
+}
---
+++ baekjoon/14888_operator_insertion.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+
+int N;
+int num[101], op[100];
+int opCount[4];
+int mx = -1000000000, mn = 1000000000;
+
+int Calc() {
+	int result = num[0];
+	for (int i = 1; i < N; i++)
+	{
+		if (op[i-1] == 1)
+			result += num[i];
+		else if (op[i-1] == 2)
+			result -= num[i];
+		else if (op[i-1] == 3)
+			result *= num[i];
+		else if (op[i-1] == 4)
+			result /= num[i];
+	}
+	return result;
+}
+
+void FillOpArray(int i) {
+	for (int type = 0; type < 4; type++)
+	{
+		if (opCount[type] > 0) {
+			
+			opCount[type]--;
+			op[i] = type + 1;
+			FillOpArray(i + 1);
+			opCount[type]++;
+		}
+	}
+	int result = Calc();
+	mx = (result > mx) ? result : mx;
+	mn = (result < mn) ? result : mn;
+}
+
+int main() 
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		cin >> num[i];
+	for (int i = 0; i < 4; i++)
+		cin >> opCount[i];
+
+	FillOpArray(0);
+
+	cout << mx << '\n' << mn;
+
+	return 0;
+}
