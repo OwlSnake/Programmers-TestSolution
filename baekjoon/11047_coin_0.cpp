eb21cf1d82f4f32a780401358ae7d7e96ad190ab
imnhk <koloke1712@gmail.com>
2019-11-16 10:32:58 +0900

    greedy

---
+++ baekjoon/11047_coin_0.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int N, K, coin[10], answer = 0;
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> N >> K;
+	for (int i = 0; i < N; i++)
+		cin >> coin[i];
+
+	for (int i = N - 1; i >= 0; i--)
+	{
+		while (K >= coin[i]) {
+			K -= coin[i];
+			answer++;
+		}
+	}
+	cout << answer;
+	return 0;
+}
---
+++ baekjoon/2217_rope.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, t, answer = 0;
+vector<int> ropeStr;
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> t;
+		ropeStr.push_back(t);
+	}
+
+	sort(ropeStr.begin(), ropeStr.end(), greater<int>());
+
+	for (int i = 0; i < N; i++)
+	{
+		int maxWeight = (i + 1) * ropeStr[i];
+		answer = (maxWeight > answer) ? maxWeight : answer;
+	}
+	
+	cout << answer;
+	return 0;
+}
