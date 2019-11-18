93982f83bfcbfc83db36bfce5c2473d10a60f03a
imnhk <koloke1712@gmail.com>
2019-11-18 23:22:09 +0900

    stack

---
+++ baekjoon/1541_lost_bracket.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void Solve()
+{
+	string exp, num;
+	cin >> exp;
+
+	int answer = 0;
+	bool IsAfterMinus = false;
+	for (int i = 0; i <= exp.length(); i++)
+	{
+		if (exp[i] == '+' || exp[i] == '-' || exp[i] == '\0') {
+			if (IsAfterMinus)
+				answer -= stoi(num);
+			else
+				answer += stoi(num);
+			num = "";
+			if (exp[i] == '-')
+				IsAfterMinus = true;
+		}
+		else
+			num += exp[i];
+	}
+	cout << answer;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	Solve();
+	return 0;
+}
---
+++ baekjoon/1874_stack_sequence.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<char> result;
+vector<int> stack;
+
+void Solve()
+{
+	int N, nextNum, lastNum = 1;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> nextNum;
+
+		if (nextNum >= lastNum) {
+			for (int j = lastNum; j <= nextNum; j++)
+			{
+				stack.push_back(j);
+				result.push_back('+');
+			}
+			stack.pop_back();
+			result.push_back('-');
+
+			lastNum = nextNum + 1;
+		}
+		else if(nextNum == stack.back()){
+			stack.pop_back();
+			result.push_back('-');
+		}
+		else {
+			cout << "NO";
+			return;
+		}
+	}
+
+	for (int i = 0; i < result.size(); i++)
+		cout << result[i] << '\n';
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	Solve();
+	return 0;
+}
