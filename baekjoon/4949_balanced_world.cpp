89ff93ed9d21bcc4d5b2872c99b444c17842715d
imnhk <koloke1712@gmail.com>
2019-11-19 15:13:59 +0900

    parenthesis

---
+++ baekjoon/4949_balanced_world.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+bool CheckParenthesis(string s) {
+	vector<char> parenthesis;
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s[i] == '(')
+			parenthesis.push_back('(');
+		else if(s[i] == '[')
+			parenthesis.push_back('[');
+		else if (s[i] == ')') {
+			if (parenthesis.size() > 0 && parenthesis.back() == '(')
+				parenthesis.pop_back();
+			else
+				return false;
+		}
+		else if (s[i] == ']') {
+			if (parenthesis.size() > 0 && parenthesis.back() == '[')
+				parenthesis.pop_back();
+			else
+				return false;
+		}
+	}
+	if (parenthesis.size() > 0)
+		return false;
+	return true;
+}
+
+void Solve()
+{
+	int N, answer = 0;
+	string str;
+	while (true)
+	{
+		string str;
+		getline(cin, str);	
+		if (str == ".")
+			break;
+		if (CheckParenthesis(str))
+			cout << "yes\n";
+		else
+			cout << "no\n";
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	Solve();
+	return 0;
+}
---
+++ baekjoon/9012_parenthesis.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+bool CheckParenthesis(string s) {
+	int openedP = 0;
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s[i] == '(')
+			openedP++;
+		else			
+			openedP--;
+
+		if (openedP < 0)
+			return false;
+	}
+	if (openedP != 0)
+		return false;
+	return true;
+}
+
+void Solve()
+{
+	int N, answer = 0;
+	vector<int> stack;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		string PS;
+		cin >> PS;
+		if (CheckParenthesis(PS))
+			cout << "YES\n";
+		else
+			cout << "NO\n";
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	Solve();
+	return 0;
+}
