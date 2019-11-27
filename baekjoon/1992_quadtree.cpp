04ec250dd7841dccffac39a94dca75714039432d
imnhk <koloke1712@gmail.com>
2019-11-27 16:24:28 +0900

    Divide papers

---
+++ baekjoon/1992_quadtree.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+using namespace std;
+
+char arr[64][64];
+string code;
+
+void CheckColor(int row, int col, int size) {
+	// Check if color of this part is identical
+	bool isColorSame = true;
+	for (int i = row; i < row + size; i++)
+	{
+		for (int j = col; j < col + size; j++) {
+			if (arr[row][col] != arr[i][j]) {
+				isColorSame = false;
+				break;
+			}
+		}
+		if (!isColorSame)
+			break;
+	}
+	// If same, add to code
+	if (isColorSame) {
+		if (arr[row][col] == '0')
+			code += "0";
+		else
+			code += "1";
+		return;
+	}
+	// If not same, divide and check
+	code += "(";
+	CheckColor(row, col, size / 2);
+	CheckColor(row, col + size / 2, size / 2);
+	CheckColor(row + size / 2, col, size / 2);
+	CheckColor(row + size / 2, col + size / 2, size / 2);
+	code += ")";
+}
+
+void Solve()
+{
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		string row;
+		cin >> row;
+		for (int j = 0; j < N; j++)
+			arr[i][j] = row[j];
+	}
+
+	CheckColor(0, 0, N);
+	cout << code;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	Solve();
+	return 0;
+}
---
+++ baekjoon/2630_dividing_paper.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+
+int arr[128][128];
+int wCount = 0, bCount = 0;
+
+void CheckColor(int row, int col, int size) {
+	// Check if color of this paper is identical
+	bool isColorSame = true;
+	for (int i = row; i < row + size; i++)
+	{
+		for (int j = col; j < col + size; j++) {
+			if (arr[row][col] != arr[i][j]) {
+				isColorSame = false;
+				break;
+			}
+		}
+		if (!isColorSame)
+			break;
+	}
+	// If same, update count
+	if (isColorSame) {
+		if (arr[row][col] == 0)
+			wCount++;
+		else
+			bCount++;
+		return;
+	}
+	// If not same, divide paper and check
+	CheckColor(row, col, size / 2);
+	CheckColor(row + size / 2, col, size / 2);
+	CheckColor(row, col + size / 2, size / 2);
+	CheckColor(row + size / 2, col + size / 2, size / 2);
+}
+
+void Solve()
+{
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			cin >> arr[i][j];
+
+	CheckColor(0, 0, N);
+
+	cout << wCount << '\n' << bCount;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	Solve();
+	return 0;
+}
