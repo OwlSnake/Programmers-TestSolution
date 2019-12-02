1080403666bb1529abd0b3699b5a35584533d627
imnhk <koloke1712@gmail.com>
2019-12-02 18:37:52 +0900

    Create 2606_virus.cpp

---
+++ baekjoon/2606_virus.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int N, M;
+int graph[101][101];
+int answer = 0;
+
+void BFS() {
+	vector<int> visited(N + 1);
+	queue<int> queue;
+
+	queue.push(1);
+	visited[1] = 1;
+
+	while (!queue.empty()) {
+		int cur = queue.front();
+		queue.pop();
+
+		for (int i = 1; i <= N; i++) {
+			if (graph[cur][i] == 1 && visited[i] == 0) {
+				queue.push(i);
+				visited[i] = 1;
+				answer++;
+			}
+		}
+	}
+}
+
+void Solve()
+{
+	// Input
+	cin >> N >> M;
+	for (int i = 0; i < M; i++)
+	{
+		int n1, n2;
+		cin >> n1 >> n2;
+		graph[n1][n2] = 1;
+		graph[n2][n1] = 1;
+	}
+
+	// Solve
+	BFS();
+
+	// Output
+	cout << answer;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	Solve();
+	return 0;
+}
