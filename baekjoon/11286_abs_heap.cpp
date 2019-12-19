c0731f8b998316bb1a01b20af628de7828cfd069
imnhk <koloke1712@gmail.com>
2019-12-19 10:26:35 +0900

    Create 11286_abs_heap.cpp

---
+++ baekjoon/11286_abs_heap.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+
+void Solve()
+{
+	int N, t;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> t;
+		if (t == 0) {
+			if (q.empty())
+				cout << "0\n";
+			else {
+				cout << q.top().second << '\n';
+				q.pop();
+			}
+		}
+		else
+			q.push(make_pair(abs(t), t));
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	Solve();
+	return 0;
+}
