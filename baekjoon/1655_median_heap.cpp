5bffdc1a290f7d0c82194ebb29da427bd0cf7802
imnhk <koloke1712@gmail.com>
2019-12-22 23:14:40 +0900

    Create 1655_median_heap.cpp

---
+++ baekjoon/1655_median_heap.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int>> minHeap; // pop smallest
+priority_queue<int, vector<int>, less<int>> maxHeap; // pop largest
+
+void Solve()
+{
+	int N, t;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> t;
+		if (minHeap.size() >= maxHeap.size())
+			maxHeap.push(t);
+		else
+			minHeap.push(t);
+
+		if (!minHeap.empty() && !maxHeap.empty() && minHeap.top() < maxHeap.top()) {
+			int greaterTop = minHeap.top();
+			int lessTop = maxHeap.top();
+			minHeap.pop();
+			maxHeap.pop();
+			minHeap.push(lessTop);
+			maxHeap.push(greaterTop);
+		}
+		cout << maxHeap.top() << '\n';
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	Solve();
+	return 0;
+}
