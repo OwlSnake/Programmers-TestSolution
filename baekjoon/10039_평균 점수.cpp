76616607c79f9f39a195e1f69ac9ff7197769fde
imnhk <koloke1712@gmail.com>
2020-01-28 20:20:21 +0900

    Rename

---
+++ baekjoon/10039_평균 점수.cpp
@@ -0,0 +1,16 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t, sum = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> t;
+		if (t < 40)
+			t = 40;
+		sum += t;
+	}
+	cout << sum / 5;
+	return 0;
+}
---
+++ baekjoon/9095_1, 2, 3 더하기.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int backtracking(int n) {
+	if (n == 0)
+		return 1;
+
+	int answer = 0;
+	for (int i = 1; i <= 3; i++)
+	{
+		if (n >= 1)
+			answer += backtracking(n - i);
+	}
+	return answer;
+}
+
+void solve() {
+	int n;
+	cin >> n;
+	cout << backtracking(n) << '\n';
+}
+
+int main() {
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++)
+		solve();
+
+	return 0;
+}
