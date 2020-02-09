c403f1b47ac9cd9e6b6f171e17c40a99cb1b2333
imnhk <koloke1712@gmail.com>
2020-02-09 23:15:59 +0900

    Create 11052_카드 구매하기.cpp

---
+++ baekjoon/11052_카드 구매하기.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int N, pack[1001], dp[1001] = { 0 };
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+		cin >> pack[i];
+
+	for (int i = 1; i <= N; i++)
+	{
+		int mx = pack[i];
+		for (int j = 1; j <= i/2; j++)
+			mx = max(dp[i-j] + dp[j], mx);
+		
+		dp[i] = mx;
+	}
+	cout << dp[N];
+
+	return 0;
+}
