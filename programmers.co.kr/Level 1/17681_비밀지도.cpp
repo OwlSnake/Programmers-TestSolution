63f085e930293b1496f9a46b93e98faa9ddefa4a
imnhk <koloke1712@gmail.com>
2020-04-05 12:55:28 +0900

    Create 17681_비밀지도.cpp

---
+++ programmers.co.kr/Level 1/17681_비밀지도.cpp
@@ -0,0 +1,33 @@
+﻿#include <string>
+#include <vector>
+
+#include <iostream>
+
+using namespace std;
+
+vector<string> answer;
+
+string convertToStr(int num, int size) {
+	string result;
+	while (num > 0) {
+		if (num % 2 == 0)
+			result = ' ' + result;
+		else
+			result = '#' + result;
+		num /= 2;
+	}
+	while (result.length() < size)
+		result = ' ' + result;
+    
+	return result;
+}
+
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
+	vector<string> answer;
+	for (int i = 0; i < n; i++)
+    {
+		arr1[i] = arr1[i] | arr2[i];
+		answer.push_back(convertToStr(arr1[i], n));
+	}
+	return answer;
+}
