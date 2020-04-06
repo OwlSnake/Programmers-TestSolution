d05722ae28e8434369fd5f5ffd81f9efd0cf0c76
imnhk <koloke1712@gmail.com>
2020-04-06 09:15:02 +0900

    Create 64061_크레인 인형뽑기 게임.cpp

---
+++ programmers.co.kr/Level 1/64061_크레인 인형뽑기 게임.cpp
@@ -0,0 +1,28 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> s;
+int answer = 0;
+
+int solution(vector<vector<int>> board, vector<int> moves) {
+	for (int move : moves) {
+		for (int i = board.size() - 1; i >= 0; i--) {
+			if (board[i][move] != 0) {
+				if (!s.empty()) {
+					if (s.back() == board[i][move]) {
+						answer++;
+						s.pop_back();
+					}
+					else {
+						s.push_back(board[i][move]);
+					}
+				}
+				board[i][move] = 0;
+			}
+		}
+	}
+
+	return answer;
+}
