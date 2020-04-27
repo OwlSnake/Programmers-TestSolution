ce3c65f48b88ef9fe682d4cd12e9aca43f8a7fa6
imnhk <koloke1712@gmail.com>
2020-04-27 23:26:10 +0900

    Create 42629_라면공장.cpp

---
+++ programmers.co.kr/Level 2/42629_라면공장.cpp
@@ -0,0 +1,40 @@
+﻿#include <string>
+#include <vector>
+#include <queue>
+
+#include <iostream>
+using namespace std;
+
+int solution(int stock, vector<int> dates, vector<int> supplies, int k) {
+	int answer = 0;
+	int curDates = 0;
+	int idx = 0;
+
+	priority_queue<int> supplyQ;
+
+	while (curDates < k)
+	{
+		cout << curDates << " date / stock " << stock << "\n";
+		if (idx < dates.size() && curDates == dates[idx]) {
+			supplyQ.push(supplies[idx]);
+			idx++;
+		}
+
+		if (stock == 0) {
+			stock += supplyQ.top();
+			supplyQ.pop();
+			answer++;
+		}
+
+		stock--;
+		curDates++;
+	}
+
+	return answer;
+}
+
+int main() {
+	solution(4, { 4,10,15 }, { 20,5,10 }, 30);
+	return 0;
+
+}
